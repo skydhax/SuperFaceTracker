a080db7dbc84dc18b27feaf73574e2d87e10c564
angelrg <danielreyesicc@gmail.com>
2017-05-28 12:36:28 -0500

    Initial Commit

---
+++ SuperFaceTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperFaceTracker
+//
+//  Created by Angel Reyes on 28/05/17.
+//  Copyright © 2017 Skylabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
